7168dc07be80a43716a3adbcfde6ea8c9cbe6988
Amin Jams <aminjam.software@gmail.com>
2015-11-13 11:40:59 -0500

    Remove /etc/hosts from assets
    Remove /etc/hosts from assets and make_tarball is now using /tmp for
    extracting the tarball for OSx compatibility
    [#107944502]
    (https://www.pivotaltracker.com/story/show/107944502)
    
    Signed-off-by: David Jahn <david.a.jahn@gmail.com>

--- bin/make_tarball.sh
+++ bin/make_tarball.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 make_tarball="
-	mkdir -p tmp/$1/etc
-	cp $1/assets/etc/hosts tmp/$1/etc/hosts
-	cp $1/assets/etc/timezone tmp/$1/etc/timezone
-	tar -C tmp/$1 -xf tmp/${1}.tar
-	ls tmp/$1/ | xargs tar -C tmp/$1 -czf ${1}.tar.gz
-	rm -rf tmp/$1
+	mkdir -p /tmp/$1/etc
+	tar -C /tmp/$1 -xf tmp/${1}.tar
+	ls /tmp/$1/ | xargs tar -C /tmp/$1 -czf ${1}.tar.gz
+	rm -rf /tmp/$1
 "
 
 if [ "$(uname)" == "Darwin" ]; then
