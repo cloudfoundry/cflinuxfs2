45fd0dff18f7ff2a7f173f3f5ac0f64303063bce
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-08-12 10:42:06 -0400

    Update spec to use <<~ operator
    [#126143515]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- spec/packages/image_magick_spec.rb
+++ spec/packages/image_magick_spec.rb
@@ -15,19 +15,19 @@ describe "ImageMagick" do
 
   context "pipe character does not execute subsequent commands" do
     let(:exploit_setup) {
-      <<-SETUP
-tee exploit.mvg > /dev/null <<EOF
-push graphic-context
-viewbox 0 0 640 480
-image copy 200,200 100,100 "|echo EXPLOITED > /tmp/exploit.txt;"
-pop graphic-context
-EOF
-      SETUP
+      <<~SETUP
+         tee exploit.mvg > /dev/null <<EOF
+         push graphic-context
+         viewbox 0 0 640 480
+         image copy 200,200 100,100 "|echo EXPLOITED > /tmp/exploit.txt;"
+         pop graphic-context
+         EOF
+         SETUP
     }
     let(:exploit_command) {
-      <<-EXPLOIT
-        convert exploit.mvg out.png; cat "/tmp/exploit.txt"
-      EXPLOIT
+      <<~EXPLOIT
+         convert exploit.mvg out.png; cat "/tmp/exploit.txt"
+         EXPLOIT
     }
 
     it "does not cause code execution" do
@@ -39,9 +39,9 @@ EOF
   context "insufficient shell character filtering" do
     let(:exploit_setup) { "echo EXPLOIT EXPLOITED > /tmp/secret.txt" }
     let(:exploit_command) {
-      <<-EXPLOIT
-        convert 'https://example.com"; cat "/tmp/secret.txt' out.png
-      EXPLOIT
+      <<~EXPLOIT
+         convert 'https://example.com"; cat "/tmp/secret.txt' out.png
+         EXPLOIT
     }
 
     it "does not cause code execution" do
@@ -52,14 +52,14 @@ EOF
 
   context "external file inclusion" do
     let(:exploit_setup) {
-      <<-SETUP
-tee exploit.mvg > /dev/null <<EOF
-push graphic-context
-viewbox 0 0 640 480
-fill 'url(http://example.com/)'
-pop graphic-context
-EOF
-      SETUP
+      <<~SETUP
+         tee exploit.mvg > /dev/null <<EOF
+         push graphic-context
+         viewbox 0 0 640 480
+         fill 'url(http://example.com/)'
+         pop graphic-context
+         EOF
+         SETUP
     }
     let(:exploit_command) { "convert exploit.mvg out.png" }
 
@@ -71,22 +71,22 @@ EOF
 
   context "using the 'ephemeral' pseudo protocol" do
     let(:exploit_setup) {
-      <<-SETUP
-touch /tmp/delete.txt
+      <<~SETUP
+         touch /tmp/delete.txt
 
-tee exploit.mvg > /dev/null <<EOF
-push graphic-context
-viewbox 0 0 640 480
-image over 0,0 0,0 'ephemeral:/tmp/delete.txt'
-popgraphic-context
-EOF
-      SETUP
+         tee exploit.mvg > /dev/null <<EOF
+         push graphic-context
+         viewbox 0 0 640 480
+         image over 0,0 0,0 'ephemeral:/tmp/delete.txt'
+         popgraphic-context
+         EOF
+         SETUP
     }
     let(:exploit_command) {
-      <<-EXPLOIT
-      convert exploit.mvg out.png
-      ls -l /tmp/delete.txt
-      EXPLOIT
+      <<~EXPLOIT
+         convert exploit.mvg out.png
+         ls -l /tmp/delete.txt
+         EXPLOIT
     }
 
     it "does not delete specified files" do
@@ -98,31 +98,31 @@ EOF
 
   context "using the 'msl' pseudo protocol" do
     let(:exploit_setup) {
-      <<-SETUP
-convert -size 2x2 canvas:khaki /tmp/malicious.gif
-echo "irreplacable content" > /tmp/irreplacable.txt
+      <<~SETUP
+         convert -size 2x2 canvas:khaki /tmp/malicious.gif
+         echo "irreplacable content" > /tmp/irreplacable.txt
 
-tee /tmp/msl.txt > /dev/null <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<image>
-<read filename="/tmp/malicious.gif" />
-<write filename="/tmp/irreplacable.txt" />
-</image>
-EOF
+         tee /tmp/msl.txt > /dev/null <<EOF
+         <?xml version="1.0" encoding="UTF-8"?>
+         <image>
+         <read filename="/tmp/malicious.gif" />
+         <write filename="/tmp/irreplacable.txt" />
+         </image>
+         EOF
 
-tee file_move.mvg > /dev/null <<EOF
-push graphic-context
-viewbox 0 0 640 480
-image over 0,0 0,0 'msl:/tmp/msl.txt'
-popgraphic-context
-EOF
-      SETUP
+         tee file_move.mvg > /dev/null <<EOF
+         push graphic-context
+         viewbox 0 0 640 480
+         image over 0,0 0,0 'msl:/tmp/msl.txt'
+         popgraphic-context
+         EOF
+         SETUP
     }
     let(:exploit_command) {
-      <<-EXPLOIT
-      convert file_move.mvg out.png
-      cat /tmp/irreplacable.txt
-      EXPLOIT
+      <<~EXPLOIT
+         convert file_move.mvg out.png
+         cat /tmp/irreplacable.txt
+         EXPLOIT
     }
 
     it "does not move files" do
@@ -133,24 +133,24 @@ EOF
 
   context "using the 'label' pseudo protocol" do
     let(:exploit_setup) {
-      <<-SETUP
-echo "234" > /tmp/secrets.txt
+      <<~SETUP
+         echo "234" > /tmp/secrets.txt
 
-tee file_read.mvg > /dev/null <<EOF
-push graphic-context
-viewbox 0 0 100 32
-image over 4,4 0,0 'label:@/tmp/secrets.txt'
-pop graphic-context
-EOF
+         tee file_read.mvg > /dev/null <<EOF
+         push graphic-context
+         viewbox 0 0 100 32
+         image over 4,4 0,0 'label:@/tmp/secrets.txt'
+         pop graphic-context
+         EOF
 
-apt-get install -y gocr > /dev/null
-      SETUP
+         apt-get install -y gocr > /dev/null
+         SETUP
     }
     let(:exploit_command) {
-      <<-EXPLOIT
-      convert file_read.mvg out.png
-      gocr out.png
-      EXPLOIT
+      <<~EXPLOIT
+         convert file_read.mvg out.png
+         gocr out.png
+         EXPLOIT
     }
 
     it "does not display the secrets as a text label in the generated image" do
