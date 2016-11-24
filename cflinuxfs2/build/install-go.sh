8620a3d838ebfb5f341b5ea8a99f4231989ec67f
Yulia Gaponenko <barthy@yandex.ru>
2016-11-24 14:07:45 +0100

    Add golang support for ppc64le

--- cflinuxfs2/build/install-go.sh
+++ cflinuxfs2/build/install-go.sh
@@ -7,18 +7,20 @@ if [ -z "$GO_VERSION" ]; then
     exit 1
 fi
 
-if [ "`uname -m`" == "ppc64le" ]; then
-  echo "Go $GO_VERSION is not available for download for ppc64le"
-else
-  pushd /tmp
+pushd /tmp
+  if [ "`uname -m`" == "ppc64le" ]; then
+    GO_TAR_FILE="go-$GO_VERSION-ppc64le.tar.gz"
+    wget "http://ftp.unicamp.br/pub/ppc64el/ubuntu/14_04/cloud-foundry/$GO_TAR_FILE"
+  else
     GO_TAR_FILE="go$GO_VERSION.linux-amd64.tar.gz"
     wget "https://buildpacks.cloudfoundry.org/concourse-binaries/go/$GO_TAR_FILE"
-    tar -xvf $GO_TAR_FILE
-    mv go /usr/local/go1.7
-  popd
+  fi
+  tar -xvf $GO_TAR_FILE
+  mv go /usr/local/go1.7
+popd
 
-  rm "/tmp/$GO_TAR_FILE"
-fi
+rm "/tmp/$GO_TAR_FILE"
+
 
 
 
