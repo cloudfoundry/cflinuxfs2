a0dd27b57feb35295923f27193995d5d8d6cda45
Yulia Gaponenko <yulia.gaponenko1@de.ibm.com>
2016-12-08 15:57:38 +0000

    Parameterize link to golang archive
    GO_URL env variable should be specified.
    Default golang link is https://buildpacks.cloudfoundry.org/concourse-binaries/go/go1.7.3.linux-amd64.tar.gz

--- cflinuxfs2/build/install-go.sh
+++ cflinuxfs2/build/install-go.sh
@@ -1,25 +1,19 @@
 set -e -x
 
-GO_VERSION=${1}
+GO_URL=${1}
 
-if [ -z "$GO_VERSION" ]; then
-    echo "usage: ${0} [VERSION]."
+if [ -z "$GO_URL" ]; then
+    echo "usage: ${0} [URL]."
     exit 1
 fi
 
 pushd /tmp
-  if [ "`uname -m`" == "ppc64le" ]; then
-    GO_TAR_FILE="go-$GO_VERSION-ppc64le.tar.gz"
-    wget "http://ftp.unicamp.br/pub/ppc64el/ubuntu/14_04/cloud-foundry/$GO_TAR_FILE"
-  else
-    GO_TAR_FILE="go$GO_VERSION.linux-amd64.tar.gz"
-    wget "https://buildpacks.cloudfoundry.org/concourse-binaries/go/$GO_TAR_FILE"
-  fi
-  tar -xvf $GO_TAR_FILE
+  wget $GO_URL
+  tar -xvf ${GO_URL##*/}
   mv go /usr/local/go1.7
 popd
 
-rm "/tmp/$GO_TAR_FILE"
+rm "/tmp/${GO_URL##*/}"
 
 
 
